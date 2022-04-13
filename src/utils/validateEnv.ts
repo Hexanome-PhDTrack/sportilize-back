87863ed47ab65d90700ef4d4787ebf925abce6fe
fitz35 <clement@lahoche.info>
2022-04-13 08:42:53 +0200

    database connection initialized

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,10 +1,17 @@
-import { cleanEnv, port } from 'envalid';
+import { cleanEnv, port, str } from 'envalid';
 import dotenv from 'dotenv';
 dotenv.config();
 
 function validateEnv() {
   cleanEnv(process.env, {
     PORT: port(),
+
+    //database
+    DATABASE_HOST: str(),
+    DATABASE_PORT: port(),
+    DATABASE_USERNAME: str(),
+    DATABASE_PASSWORD: str(),
+    DATABASE_NAME: str(),
   });
 }
 
