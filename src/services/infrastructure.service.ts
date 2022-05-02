228ff43fae382cada41f63fdb84b8f099108ae0b
fitz35 <clement@lahoche.info>
2022-05-02 09:36:27 +0200

    add tests

--- src/services/infrastructure.service.ts
+++ src/services/infrastructure.service.ts
@@ -3,6 +3,30 @@ import { DataSource, Repository } from 'typeorm';
 import { AppDataSource } from '../data-source';
 import { Point } from 'geojson';
 
+function calculDist(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number,
+): number {
+  const R = 6371e3; // metres
+  const sigma1 = (lat1 * Math.PI) / 180; // φ, λ in radians
+  const sigma2 = (lat2 * Math.PI) / 180;
+  const deltaSigma = ((lat2 - lat1) * Math.PI) / 180;
+  const deltaLambda = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(deltaSigma / 2) * Math.sin(deltaSigma / 2) +
+    Math.cos(sigma1) *
+      Math.cos(sigma2) *
+      Math.sin(deltaLambda / 2) *
+      Math.sin(deltaLambda / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  const d = R * c; // in meter
+  return d;
+}
+
 class InfrastructureService {
   public dbConnection: DataSource;
   public infrastructureRepo: Repository<InfrastructureEntity>;
@@ -28,24 +52,32 @@ class InfrastructureService {
    * @param closed
    * @returns
    */
-  public async getEventsByArea(
+  public async getInfrasByArea(
     positionRef: Point,
     distance: number,
-  ): Promise<InfrastructureEntity> {
+  ): Promise<Array<InfrastructureEntity>> {
     // https://aaronfrancis.com/2021/efficient-distance-querying-in-my-sql#:~:text=Calculating%20Distance%20in%20MySQL&text=The%20TL%3BDR%20of%20it,--%20Returns%20distance%20in%20meters.
-    const infra: InfrastructureEntity = await this.infrastructureRepo
-      .createQueryBuilder('infrastructure')
-      .where(
-        'ST_Distance_Sphere(point(ST_Latitude(coordinates), ST_Longitude(coordinates)), point(:latitude, :longitude)) < :distance',
-        {
-          latitude: positionRef.coordinates[0],
-          longitude: positionRef.coordinates[1],
-          distance: distance,
-        },
-      )
-      .getOne();
+    const allInfra: Array<InfrastructureEntity> =
+      await this.infrastructureRepo.find();
+    let resultInfra: Array<InfrastructureEntity> = new Array();
+    for (let i: number; i < allInfra.length; i++) {
+      const infra: InfrastructureEntity = allInfra[i];
+      const pointInfra: Point = JSON.parse(infra.point);
+      if (
+        Math.abs(
+          calculDist(
+            positionRef.coordinates[0],
+            positionRef.coordinates[1],
+            pointInfra.coordinates[0],
+            pointInfra.coordinates[1],
+          ),
+        ) <= distance
+      ) {
+        resultInfra.push(infra);
+      }
+    }
 
-    return infra;
+    return resultInfra;
   }
 }
 
--- tests/api/event.test.ts
+++ tests/api/event.test.ts
@@ -1,15 +1,89 @@
 import fetch, { RequestInit } from 'node-fetch-commonjs';
 import { UserEntity } from '../../src/databaseEntities/UserEntity';
 import { UserAuthEntity } from '../../src/databaseEntities/UserAuthEntity';
+import CreateEventInput from '../../src/inputClasses/CreateEventInput';
 import LoginDto from '../../src/dataTransfertObject/LoginDto';
 
 const BASE_URL = 'http://localhost:3000/api';
 const API_VESRION = 'v1';
 const endpoint = 'events';
+const authEndpoint = 'auth';
 
 describe('event create API endpoint test', () => {
   const resource = 'create';
   it("should fail because we aren't connected.", async () => {
     const url = `${BASE_URL}/${API_VESRION}/${endpoint}/${resource}`;
+
+    const createEvent: CreateEventInput = {
+      infrastructureId: 10,
+      nbMaxParticipants: 10,
+      sports: ['judo'],
+      description: 'untest',
+      beginDate: Date.now().toString(),
+      endDate: (Date.now() + 60 * 1000).toString(),
+    };
+
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(createEvent),
+    };
+
+    const response = await fetch(url, options);
+    expect(response.status).toBe(403);
+  });
+
+  it('Should connect and create a new event.', async () => {
+    const url = `${BASE_URL}/${API_VESRION}/${endpoint}/${resource}`;
+
+    // connection
+    const resourceCon = 'login';
+    const urlCon = `${BASE_URL}/${API_VESRION}/${authEndpoint}/${resourceCon}`;
+    const userData: LoginDto = {
+      password: 'jestTest123',
+      email: 'test832602.1906488243@mail.com',
+    };
+
+    const optionsCon: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    };
+
+    const response = await fetch(urlCon, optionsCon);
+    let jsonRes;
+    try {
+      jsonRes = await response.json();
+    } catch (e) {
+      console.log(e);
+    }
+    expect(response.status).toBe(200);
+    let cookie = response.headers.get('Set-Cookie');
+    expect(cookie.split('Max-Age=')[1]).toBe('3600');
+
+    // create event
+    const createEvent: CreateEventInput = {
+      infrastructureId: 10,
+      nbMaxParticipants: 10,
+      sports: ['judo'],
+      description: 'untest',
+      beginDate: Date.now().toString(),
+      endDate: (Date.now() + 60 * 1000).toString(),
+    };
+
+    const options: RequestInit = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(createEvent),
+    };
+
+    const response = await fetch(url, options);
+    expect(response.status).toBe(403);
   });
 });
